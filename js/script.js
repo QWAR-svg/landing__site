b8e97bdc9b56f1dbf233559892a152df09a4bf48
QWAR-svg <zaz765831@gmail.com>
2021-08-14 12:31:54 +0300

    js and scss

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-const btn  = document.querySelector('button');
+const btn__right  = document.querySelector('.btn__right');
+const btn__left  = document.querySelector('.btn__left');
 const slide  = document.querySelector('.slide');
-// debugger;
 const array = ['first', 'second', 'third'];
+const blocks = document.querySelectorAll('.info');
+
 let z = 0;
-btn.addEventListener('click', () => {     
+btn__right.addEventListener('click', () => {     
     slide.classList.remove("first", "second", "third");
+    
     if (z < 2) {
         slide.classList.add(array[++z]); 
     } else {
         z = 0;
         slide.classList.add(array[z]);
     }
+
+    showBlock(z) 
 })
+
+btn__left.addEventListener('click', () => {     
+    slide.classList.remove("first", "second", "third");
+    if (z <= 0) {
+        z = 2;
+        slide.classList.add(array[z]); 
+    } else {
+        slide.classList.add(array[--z]);
+    }
+    showBlock(z) 
+})
+
+function showBlock(i) {
+    blocks.forEach(item => {
+        item.classList.remove("active");
+    });
+
+    blocks[i].classList.add("active");
+}
+
